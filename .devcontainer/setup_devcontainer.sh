fc591472b4c9656c17b3eb563c70c21a4b10ecca
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 21:29:08 +0000

    added pre-commit

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -5,3 +5,4 @@ echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
 
 pip install -r requirements.txt
+pre-commit install
