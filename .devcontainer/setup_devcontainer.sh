f477276f6bc9b18ded1408220e36281a7ca10c54
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 20:11:32 +0000

    package

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "source $(pwd)/ros_settings.sh" >> ~/.bashrc
+echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
---
+++ src/jair_turtlesim/jair_turtlesim/follower.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from jair_turtlesim.')
+
+
+if __name__ == '__main__':
+    main()
