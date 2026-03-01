dc93919989889c8942020ea1435f708fac24f5ca
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 19:24:39 +0000

    fix

--- ros_settings.sh
+++ ros_settings.sh
@@ -4,6 +4,3 @@ source /opt/ros/humble/setup.sh
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 eval "$(register-python-argcomplete3 ros2 2>/dev/null || register-python-argcomplete ros2)"
 eval "$(register-python-argcomplete3 colcon 2>/dev/null || register-python-argcomplete colcon)"
-
-export ROS_LOCALHOST_ONLY=0
-# export DISPLAY=:0
