3102af81fe790c40f6ddd1a8a571c0eb8d9fea29
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 14:08:19 +0000

    rqt

--- ros_settings.sh
+++ ros_settings.sh
@@ -5,6 +5,5 @@ source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 eval "$(register-python-argcomplete3 ros2 2>/dev/null || register-python-argcomplete ros2)"
 eval "$(register-python-argcomplete3 colcon 2>/dev/null || register-python-argcomplete colcon)"
 
-export ROS_DOMAIN_ID=0
 export ROS_LOCALHOST_ONLY=0
 # export DISPLAY=:0
