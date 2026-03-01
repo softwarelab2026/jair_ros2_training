32f9aad5bb5f545d48b896f297d0b3fc87fecbde
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 12:46:51 +0000

    added ros bash completions

--- ros_settings.sh
+++ ros_settings.sh
@@ -1,4 +1,9 @@
 
 source /opt/ros/humble/setup.sh
+
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+eval "$(register-python-argcomplete3 ros2 2>/dev/null || register-python-argcomplete ros2)"
+eval "$(register-python-argcomplete3 colcon 2>/dev/null || register-python-argcomplete colcon)"
+
 export ROS_DOMAIN_ID=0
 export ROS_LOCALHOST_ONLY=0
