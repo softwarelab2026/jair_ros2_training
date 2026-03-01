306454d3ddfb69762b8d90baf7fbb236cbbe35b7
Jair-F <jair.fehlauer@gmail.com>
2026-03-01 22:02:55 +0000

    making circle on image

--- src/jair_turtlesim/image_feeder/image_feeder.py
+++ src/jair_turtlesim/image_feeder/image_feeder.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -6,6 +8,8 @@ from std_msgs.msg import String
 
 class ImageFeeder(Node):
     FPS = 30
+    IMG_WIDTH = 500
+    IMG_HEIGHT = 800
 
     def __init__(self) -> None:
         super().__init__('image_feeder_raw')
@@ -14,8 +18,24 @@ class ImageFeeder(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
-    def _create_img(self) -> None:
-        pass
+        img = self._create_img(self.IMG_WIDTH, self.IMG_HEIGHT)
+        self._show_image(img)
+
+    def _show_image(self, img) -> None:
+        cv2.imshow('Black Image', img)
+        # cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def _create_img(self, height: int, width: int) -> None:
+        black_img = np.zeros((height, width, 3), np.uint8)
+
+        circle_pos = (int(height / 2), int(width / 2))
+        radius = 20
+        color = (0, 255, 0)
+        thickness = -1
+        cv2.circle(black_img, circle_pos, radius, color, thickness)
+
+        return black_img
 
     def timer_callback(self) -> None:
         msg = String()
