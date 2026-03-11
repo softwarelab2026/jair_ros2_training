bcbf74f5f38a83a4cc9c01ce11dd5fae9af9b9d8
Jair-F <jair.fehlauer@gmail.com>
2026-03-11 11:54:52 +0000

    pos extract test

---
+++ src/jair_turtlesim/test/test_ball_pos_extract.py
@@ -0,0 +1,59 @@
+
+from image_feeder.image_generator import ImageGenerator
+from follower.ball_tracker import BallTracker
+
+def test_ball_detection_image_center(image_generator:ImageGenerator, ball_tracker:BallTracker):
+    center = int(image_generator.image_width / 2), int(image_generator.image_height / 2)
+    
+    image_generator._ball_pos_x = center[0]
+    image_generator._ball_pos_y = center[1]
+
+    img = image_generator._render_current_state()
+    ball_pos = ball_tracker._extract_ball_pos(img)
+    
+    assert ball_pos == center
+
+def test_ball_detection_image_left_up(image_generator:ImageGenerator, ball_tracker:BallTracker):
+    exptectd_ball_pos = 200, 200
+    
+    image_generator._ball_pos_x = exptectd_ball_pos[0]
+    image_generator._ball_pos_y = exptectd_ball_pos[1]
+
+    img = image_generator._render_current_state()
+    ball_pos = ball_tracker._extract_ball_pos(img)
+    
+    assert ball_pos == exptectd_ball_pos
+
+def test_ball_detection_image_left_down(image_generator:ImageGenerator, ball_tracker:BallTracker):
+    exptectd_ball_pos = 200, 600
+    
+    image_generator._ball_pos_x = exptectd_ball_pos[0]
+    image_generator._ball_pos_y = exptectd_ball_pos[1]
+
+    img = image_generator._render_current_state()
+    ball_pos = ball_tracker._extract_ball_pos(img)
+    
+    assert ball_pos == exptectd_ball_pos
+
+def test_ball_detection_image_right_up(image_generator:ImageGenerator, ball_tracker:BallTracker):
+    exptectd_ball_pos = 700, 600
+    
+    image_generator._ball_pos_x = exptectd_ball_pos[0]
+    image_generator._ball_pos_y = exptectd_ball_pos[1]
+
+    img = image_generator._render_current_state()
+    ball_pos = ball_tracker._extract_ball_pos(img)
+    
+    assert ball_pos == exptectd_ball_pos
+
+def test_ball_detection_image_right_down(image_generator:ImageGenerator, ball_tracker:BallTracker):
+    exptectd_ball_pos = 700, 600
+    
+    image_generator._ball_pos_x = exptectd_ball_pos[0]
+    image_generator._ball_pos_y = exptectd_ball_pos[1]
+
+    img = image_generator._render_current_state()
+    ball_pos = ball_tracker._extract_ball_pos(img)
+    
+    assert ball_pos == exptectd_ball_pos
+
