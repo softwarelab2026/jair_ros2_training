220dba5f7d1bb6f12e59c830887dd57ed230998d
Jair-F <jair.fehlauer@gmail.com>
2026-03-11 23:02:19 +0000

    added docker compose

--- src/run.py
+++ src/run.py
@@ -12,11 +12,9 @@ def generate_launch_description() -> LaunchDescription:
                 arguments=['--ros-args', '--log-level', 'fatal'],
             ),
             launch_ros.actions.Node(package='rqt_console', executable='rqt_console', output='screen'),
-            launch_ros.actions.Node(package='rqt_gui', executable='rqt_gui', output='screen'),
+            # launch_ros.actions.Node(package='rqt_gui', executable='rqt_gui', output='screen'),
+            launch_ros.actions.Node(package='rqt_publisher', executable='rqt_publisher', output='screen'),
             launch_ros.actions.Node(package='jair_turtlesim', executable='follower', output='screen'),
             launch_ros.actions.Node(package='jair_turtlesim', executable='image_feeder', output='screen'),
         ],
     )
-
-
-# clear ; RCUTILS_LOGGING_SEVERITY_THRESHOLD=ERROR ros2 launch run.py
---
+++ src/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+ros2 launch run.py
