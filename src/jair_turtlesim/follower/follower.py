232c0a379b042ac1da784fc9f0981806c1263dc3
Jair-F <jair.fehlauer@gmail.com>
2026-03-11 22:03:56 +0000

    pre-commit fixes

--- src/jair_turtlesim/follower/follower.py
+++ src/jair_turtlesim/follower/follower.py
@@ -9,23 +9,22 @@ from turtlesim.msg import Pose
 class Follower(Node):
     def __init__(self) -> None:
         super().__init__('turtle_follower')
-        self.sub_img = self.create_subscription(Image, 'camera/raw_image', self._handle_image, 10)
-        self.sub_pos = self.create_subscription(Pose, '/turtle1/pose', self._get_turtle_pose, 10)
+        self.sub_img = self.create_subscription(Image, 'camera/raw_image', self.handle_image, 10)
+        self.sub_pos = self.create_subscription(Pose, '/turtle1/pose', self.get_turtle_pose, 10)
         self.pub_turtle_vel = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
         self.turtle_pos = Pose()
         self._ball_tracker = BallTracker(self.get_logger())
 
-    def _handle_image(self, frame: Image) -> None:
+    def handle_image(self, frame: Image) -> None:
         gas, steer = self._ball_tracker.track_ball(frame, self.turtle_pos)
 
         twist_msg = Twist()
         twist_msg.angular.z = float(steer)
         twist_msg.linear.x = float(gas)
-        # twist_msg.linear.y = 1.0 # to the side
         self.pub_turtle_vel.publish(twist_msg)
 
-    def _get_turtle_pose(self, turtle_pos: Pose) -> None:
+    def get_turtle_pose(self, turtle_pos: Pose) -> None:
         self.turtle_pos = turtle_pos
 
 
--- src/jair_turtlesim/image_feeder/image_feeder.py
+++ src/jair_turtlesim/image_feeder/image_feeder.py
@@ -9,6 +9,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 
 
+# pylint: disable=too-few-public-methods
 class ImageFeeder(Node):
     FPS = 25
 
@@ -16,7 +17,7 @@ class ImageFeeder(Node):
         super().__init__('image_feeder_raw')
         self.publisher_ = self.create_publisher(Image, 'camera/raw_image', 10)
         timer_period = 1 / self.FPS
-        self.timer = self.create_timer(timer_period, self._publish_img)
+        self.timer = self.create_timer(timer_period, self.publish_img)
 
         self._img_generator = ImageGenerator(width=800, height=800, ball_radius=20)
 
@@ -27,7 +28,7 @@ class ImageFeeder(Node):
         cv2.imshow('Image', img)
         cv2.waitKey(1)
 
-    def _publish_img(self) -> None:
+    def publish_img(self) -> None:
         img = self._img_generator.render_frame()
         self.publisher_.publish(CvBridge().cv2_to_imgmsg(img))
         self._show_image(img)
