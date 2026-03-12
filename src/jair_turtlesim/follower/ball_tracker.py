a119c2288efb8c97cf2828759f6f959977284108
Jair-F <jair.fehlauer@gmail.com>
2026-03-12 13:39:26 +0000

    refactor fixes

--- src/jair_turtlesim/follower/ball_tracker.py
+++ src/jair_turtlesim/follower/ball_tracker.py
@@ -14,21 +14,21 @@ def _ros_img_to_cv2(_frame: Image) -> np.ndarray:
 # pylint: disable=too-few-public-methods
 class BallTracker:
     def __init__(self, logger: RcutilsLogger):
-        self.pid_linear = PID(2, 0.05, 0.01, 10)
-        self.pid_angular = PID(12, 0.05, 0.01, 10)
+        self._pid_linear = PID(2, 0.05, 0.01, 10)
+        self._pid_angular = PID(12, 0.05, 0.01, 10)
 
         self._logger = logger
 
-    def track_ball(self, _frame: Image, turtle_pos: Pose, dt: float) -> tuple[float, float]:
+    def track_ball(self, frame: Image, turtle_pos: Pose, dt: float) -> tuple[float, float]:
         turtle_angular_err, turtle_linear_err = calc_turtle_error(
-            _ros_img_to_cv2(_frame),
-            _frame.width,
-            _frame.height,
+            _ros_img_to_cv2(frame),
+            frame.width,
+            frame.height,
             turtle_pos,
         )
 
-        gas = self.pid_linear.calc(turtle_linear_err, dt)
-        steer = self.pid_angular.calc(turtle_angular_err, dt)
+        gas = self._pid_linear.calc(turtle_linear_err, dt)
+        steer = self._pid_angular.calc(turtle_angular_err, dt)
 
         if self._logger:
             self._logger.info(f'gas, steer: {round(gas, 2)},\t{round(steer, 2)} rad')
