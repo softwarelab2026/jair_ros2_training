f8e00631ee33deb95642b53a38a27e9a1f7554f0
Jair-F <jair.fehlauer@gmail.com>
2026-03-12 11:58:41 +0000

    building only in src/ not in every dir

--- .devcontainer/setup_devcontainer.sh
+++ .devcontainer/setup_devcontainer.sh
@@ -8,3 +8,7 @@ echo "export _colcon_cd_root=/opt/ros/humble/" >> ~/.bashrc
 pip install -r requirements.txt
 pre-commit install
 pre-commit run
+
+source ~/.bashrc
+cd src && rm -rf build install log && colcon build --symlink-install && cd -
+source src/install/setup.bash
